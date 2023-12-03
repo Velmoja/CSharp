e7cf9931aaa6a3a68b4c72fe10b2a79cbfff26ec
Velmoja <velmartem@mail.ru>
2023-12-03 20:23:09 +0500

    Complete first task

---
+++ MaximTechnology/MaximTechnology/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+        
+        Console.WriteLine(IsEvenLength(input) ? ReverseHalves(input) : ConcatenateWithReversed(input));
+    }
+    static bool IsEvenLength(string str) => str.Length % 2 == 0;
+
+    static string Reverse(string str) => new string(str.ToCharArray().Reverse().ToArray());
+
+    static string ReverseHalves(string str)
+    {
+        int midpoint = str.Length / 2;
+        return Reverse(str.Substring(0, midpoint)) + Reverse(str.Substring(midpoint));
+    }
+    
+    static string ConcatenateWithReversed(string str) => str + Reverse(str);
+}
