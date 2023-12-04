11f2582cd112b355bcd3779659ce5dd900f2e17d
Velmoja <velmartem@mail.ru>
2023-12-04 22:50:32 +0500

    Complete Task2

--- MaximTechnology/MaximTechnology/Program.cs
+++ MaximTechnology/MaximTechnology/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
-
+using System.Linq;
 class Program
 {
     static void Main()
     {
         Console.Write("Введите строку: ");
         string input = Console.ReadLine();
+        char[] invalidCharacters = EnglishOrNot(input);
         
-        Console.WriteLine(IsEvenLength(input) ? ReverseHalves(input) : ConcatenateWithReversed(input));
+        if (invalidCharacters.Length == 0)
+        {
+            Console.WriteLine(IsEvenLength(input) ? ReverseHalves(input) : ConcatenateWithReversed(input));
+        }
+        else
+        {
+            Console.Write("Ошибка! Были введены неподходящие символы: ");
+            Console.Write(string.Join(" ", invalidCharacters));
+        }
     }
+
     static bool IsEvenLength(string str) => str.Length % 2 == 0;
 
     static string Reverse(string str) => new string(str.ToCharArray().Reverse().ToArray());
@@ -18,6 +28,17 @@ class Program
         int midpoint = str.Length / 2;
         return Reverse(str.Substring(0, midpoint)) + Reverse(str.Substring(midpoint));
     }
-    
+
     static string ConcatenateWithReversed(string str) => str + Reverse(str);
+
+    static char[] EnglishOrNot(string str)
+    {
+        string inputString = str;
+        string engLngStr = "abcdefghijklmnopqrstuvwxyz";
+        char[] engLngChr = engLngStr.ToCharArray();
+
+        char[] extraCharacters = inputString.ToCharArray().Where(c => !engLngChr.Contains(c)).ToArray();
+        
+        return extraCharacters;
+    }
 }
