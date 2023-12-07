c2c77dcdc55a8523fd342896836d1a89b783dca0
Velmoja <velmartem@mail.ru>
2023-12-08 00:50:07 +0500

    Complete to Task5

--- MaximTechnology/MaximTechnology/Program.cs
+++ MaximTechnology/MaximTechnology/Program.cs
@@ -3,113 +3,68 @@ using System.Linq;
 
 class Program
 {
-    
     static void Main()
     {
-        string inversStr = "";;
-        Console.Write("Введите строку: ");
-        string input = Console.ReadLine();
-        char[] invalidCharacters = EnglishOrNot(input);
+        StringManipulation stringManipulation = new StringManipulation();
+        Sorts sorts = new Sorts();
+        TreeSort treeSort = new TreeSort();
         
-        if (input.Length % 2 != 0)
-        {
-            inversStr = ConcatenateWithReversed(input);
-        }
-        else
-        {
-            inversStr = ReverseHalves(input);
-        }
-
+        string input = stringManipulation.GetUserInput();
+        char[] invalidCharacters = stringManipulation.EnglishOrNot(input);
+        
         if (invalidCharacters.Length == 0)
         {
-            Console.WriteLine(IsEvenLength(input) ? ReverseHalves(input) : ConcatenateWithReversed(input));
+            if (stringManipulation.IsEvenLength(input))
+            {
+                Console.WriteLine(stringManipulation.ReverseHalves(input));
+            }
+            else
+            {
+                Console.WriteLine(stringManipulation.ConcatenateWithReversed(input));
+            }
             
-            Dictionary<char, int> charCount = CountCharacters(inversStr);
+            string inversStr = stringManipulation.IsEvenLength(input) ? stringManipulation.ReverseHalves(input) : stringManipulation.ConcatenateWithReversed(input);
+            
+            CharacterCounter characterCounter = new CharacterCounter();
+            Dictionary<char, int> charCount = characterCounter.CountCharacters(inversStr);
+
             Console.WriteLine("Символы и их количество в строке:");
             foreach (var entry in charCount)
             {
                 Console.WriteLine($"'{entry.Key}' - {entry.Value}");
             }
+
             Console.Write("Самая длинная подстрока начинающаяся и заканчивающаяся на гласную: ");
-            string subStr = FindLongestSubs(inversStr);
+            string subStr = stringManipulation.FindLongestSubs(inversStr);
             Console.Write(subStr);
+            
+            char[] masForSort = inversStr.ToCharArray();
+
+            Console.WriteLine("Выберите тип сортировки:");
+            Console.WriteLine("1. Быстрая сортировка (Quicksort)");
+            Console.WriteLine("2. Сортировка деревом (Tree sort)");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    sorts.RunQuickSort(masForSort);
+                    break;
+                case 2:
+                    treeSort.RunTreeSort(masForSort);
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор.");
+                    break;
+            }
         }
         else
         {
             Console.Write("Ошибка! Были введены неподходящие символы: ");
             Console.Write(string.Join(" ", invalidCharacters));
         }
+
     }
-
-    static bool IsEvenLength(string str) => str.Length % 2 == 0;
-
-    static string Reverse(string str) => new string(str.ToCharArray().Reverse().ToArray());
-
-    static string ReverseHalves(string str)
-    {
-        int midpoint = str.Length / 2;
-        return Reverse(str.Substring(0, midpoint)) + Reverse(str.Substring(midpoint));
-    }
-
-    static string ConcatenateWithReversed(string str) => str + Reverse(str);
-
-    static char[] EnglishOrNot(string str)
-    {
-        string inputString = str;
-        string engLngStr = "abcdefghijklmnopqrstuvwxyz";
-        char[] engLngChr = engLngStr.ToCharArray();
-
-        char[] extraCharacters = inputString.ToCharArray().Where(c => !engLngChr.Contains(c)).ToArray();
-        
-        return extraCharacters;
-    }
-
-    static Dictionary<char, int> CountCharacters(string str)
-    {
-        Dictionary<char, int> charCount = new Dictionary<char, int>();
-
-        foreach (char c in str)
-        {
-            if (charCount.ContainsKey(c))
-            {
-                charCount[c]++;
-            }
-            else
-            {
-                charCount[c] = 1;
-            }
-        }
-
-        return charCount;
-    }
-
-    static string FindLongestSubs(string str)
-    {
-        string vowels = "aeiou";
-        char[] first = vowels.ToCharArray();
-        string input = str;
-        
-        int indexFirst = input.IndexOfAny(first);
-        int indexSecond = input.LastIndexOfAny(first);
-
-        if(indexFirst >= 0 && indexSecond >= 0)
-        {
-            string substring = input.Substring(indexFirst, indexSecond - indexFirst + 1);
-            if (substring.Length >= 2)
-            {
-                Console.WriteLine(substring);
-            }
-            else
-            {
-                Console.Write("введен один галсный символ - ");
-                Console.Write(substring);
-            }
-        }
-        else
-        {
-            Console.WriteLine("не введено ни одной гласной буквы");
-        }
-        
-        return null;
-    }
+    
 }
