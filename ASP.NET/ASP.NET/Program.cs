aa2e45db3b48c0a2bb2221e15a26a32c33046075
Velmoja <velmartem@mail.ru>
2023-12-10 21:05:40 +0500

    Complete to Task9

--- ASP.NET/ASP.NET/Program.cs
+++ ASP.NET/ASP.NET/Program.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+int parallelLimit = builder.Configuration.GetValue<int>("AppSettings:Settings:ParallelLimit", 10);
+var semaphore = new SemaphoreSlim(parallelLimit);
 
+builder.Services.AddSingleton(semaphore);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +26,10 @@ builder.Services.AddSingleton<RandomApiBoundaryProvider>(provider =>
 
 var app = builder.Build();
 
+// Counter for current requests
+int currentRequests = 0;
+object lockObject = new object();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -33,6 +40,35 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    var sem = context.RequestServices.GetRequiredService<SemaphoreSlim>();
+
+    lock (lockObject)
+    {
+        if (currentRequests >= parallelLimit)
+        {
+            context.Response.StatusCode = 503;
+            context.Response.WriteAsync("HTTP ошибку 503 Service Unavailable. Service Unavailable.");
+            return;
+        }
+
+        currentRequests++;
+    }
+
+    try
+    {
+        await next();
+    }
+    finally
+    {
+        lock (lockObject)
+        {
+            currentRequests--;
+        }
+    }
+});
 
 app.MapControllers();
 
